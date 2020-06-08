4c2d4b6975d28b4ff9d39f917546eeeacac9a184
cypressville <62818099+cypressville@users.noreply.github.com>
2020-06-07 17:57:00 -0700

    Create Plot3.R

---
+++ figure/Plot3.R
@@ -0,0 +1,22 @@
+download <- download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", destfile = "temp")
+unzip("temp")
+unlink("temp")
+
+#reading data
+hpc <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, quote= "", strip.white=TRUE, stringsAsFactors = FALSE, na.strings= "?")
+
+# add a column of datetime
+hpc$DateTime <- strptime(paste(hpc$Date, hpc$Time), "%d/%m/%Y %H:%M:%S")
+hpc$Date <- as.Date(hpc$Date, "%d/%m/%Y")
+
+#subset the data by the date range
+hpc_sub <- subset(hpc, (hpc$Date == "2007-02-01" | hpc$Date== "2007-02-02"))
+
+
+#Plot3:
+png("plot3.png", width = 480, height = 480)
+plot  (hpc_sub$DateTime, hpc_sub$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
+points(hpc_sub$DateTime, hpc_sub$Sub_metering_2, type = "l", col = "red")
+points(hpc_sub$DateTime, hpc_sub$Sub_metering_3, type = "l", col = "blue")
+legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
